9b04f3f6abf354c7f986e282147ed6c103d62419
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-09-27 16:38:18 -0700

    added tone util and key store skeleton

---
+++ w7d3/OrganGrinder/app/assets/javascripts/stores/KeyStore.js
@@ -0,0 +1,6 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var EventEmitter = require('events').EventEmitter;
+// var TodoConstants = require('../constants/TodoConstants');
+var assign = require('object-assign');
+
+var _keys = {};
---
+++ w7d3/OrganGrinder/app/assets/javascripts/util/tones.js
@@ -0,0 +1,4 @@
+(function(){
+  window.TONES = 600;
+
+})();
