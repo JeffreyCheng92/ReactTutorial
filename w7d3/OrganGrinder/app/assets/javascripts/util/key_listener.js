51a5f879d925559ee1a7c63d454ec3bbfaa52f00
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-09-29 11:54:57 -0700

    add key listener

--- w7d3/OrganGrinder/app/assets/javascripts/util/key_listener.js
+++ w7d3/OrganGrinder/app/assets/javascripts/util/key_listener.js
@@ -1,12 +1,40 @@
 (function() {
+  var NOTE_MAP = {
+    65: 'C3',  // 'a'
+    83: 'D3',  // 's'
+    68: 'E3',  // 'd'
+    70: 'F3',  // 'f'
+    74: 'G3',  // 'j'
+    75: 'A3',  // 'k'
+    76: 'B3',  // 'l'
+    186: 'C4', // ';'
+  };
+
+  var _heldKeys = [];
+  var _validKeys = Object.keys(NOTE_MAP);
 
   document.addEventListener("keydown", function(event) {
     event.preventDefault();
-    console.log(event.keyCode);
+    var code = event.keyCode;
+    var valid = _validKeys.indexOf(code.toString()) >= 0;
+
+    if (_heldKeys.indexOf(code) === -1 && valid) {
+      _heldKeys.push(code);
+      KeyActions.keyDown(NOTE_MAP[code]);
+    }
+
   });
 
   document.addEventListener("keyup", function(event) {
     event.preventDefault();
-    console.log(event.keyCode);
+    var code = event.keyCode;
+    var idx = _heldKeys.indexOf(code);
+
+    if (idx >= 0) {
+      _heldKeys.splice(idx, 1);
+      KeyActions.keyUp(NOTE_MAP[code]);
+    }
+
   });
+
 })();
