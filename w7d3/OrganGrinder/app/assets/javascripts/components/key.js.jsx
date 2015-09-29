3e0c55b5ed38a9a5ef00f8f813aa9e7f328237d1
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-09-29 14:18:33 -0700

    added key componenet

--- w7d3/OrganGrinder/app/assets/javascripts/components/key.js.jsx
+++ w7d3/OrganGrinder/app/assets/javascripts/components/key.js.jsx
@@ -1,12 +1,36 @@
 var Key = React.createClass({
+  getInitialState: function() {
+    return { pressed: this.keyPressed };
+  },
+
   componentDidMount: function () {
+    this.note = new Note(TONES[this.props.noteName]);
     KeyStore.addChangeListener(this._onChange);
+  },
+
+  keyPressed: function() {
+    var keys = KeyStore.all();
+    return keys.indexOf(this.props.note) !== -1;
+  },
+
+  _onChange: function() {
+    var pressed = this.keyPressed;
+    if (pressed) {
+      this.note.stop();
+    } else {
+      this.note.start();
+    }
+    this.setState({ pressed: pressed });
   },
 
   render: function() {
 
     return (
-
+      <div>
+        { this.props.note }
+      </div>
     )
-  }
+  },
+
+
 });
